fd541832cf3cd196e376bb3196993434d3ebeff4
nightzillla <81122802+nightzillla@users.noreply.github.com>
2023-01-15 21:14:31 -0600

    relearning javascript fundamentals

---
+++ src/js/fundamentals_of_js.js
@@ -0,0 +1,23 @@
+/**
+ ** Important information is highlighted
+*/
+/**
+ *! Deprecated method, do not use
+ */
+/**
+ * ? Should this method be exposed in the public API?
+ */
+/**
+ * TODO: refactor this method so that is conforms to the API
+ */
+// JS Variables
+// Create a Variable called carName, assign the value Volvo to it.
+let carName = "Volvo";
+
+console.log("I created a variable and declared the value of = "+carName);
+
+// JS Operators
+function multiply(multiNum1, multiNum2){
+    return multiNum1 * multiNum2;
+}
+console.log("Multiplication function " + multiply(4,5));
