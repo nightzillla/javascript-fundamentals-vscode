4053c7778ee3845253ae8112d3493e0f4d0a7564
nightzillla <81122802+nightzillla@users.noreply.github.com>
2023-01-15 21:31:39 -0600

    relearning data types

--- src/js/fundamentals_of_js.js
+++ src/js/fundamentals_of_js.js
@@ -10,14 +10,42 @@
 /**
  * TODO: refactor this method so that is conforms to the API
  */
-// JS Variables
+/**
+ ** JS Variables 
+*/
 // Create a Variable called carName, assign the value Volvo to it.
 let carName = "Volvo";
 
 console.log("I created a variable and declared the value of = "+carName);
 
 // JS Operators
+// Multiply
 function multiply(multiNum1, multiNum2){
     return multiNum1 * multiNum2;
 }
 console.log("Multiplication function " + multiply(4,5));
+
+// Division
+
+function division(divisionNum1, divisonNum2){
+    return divisionNum1 / divisonNum2;
+}
+console.log("This is a division function " + division(12, 2));
+
+// Remainder 
+
+function remainder(remainderNum1, remainderNum2) {
+    return remainderNum1 % remainderNum2;
+}
+console.log("This is the remainder function" + remainder(15, 9));
+
+/** 
+ ** JS Data Types 
+ */
+                          // DATA TYPE
+let length = 16;          // NUMBER
+let lastName = "Johnson"; // STRING
+const x = {
+    firstName: "John",
+    lastName: "Doe"
+};                        // Object
