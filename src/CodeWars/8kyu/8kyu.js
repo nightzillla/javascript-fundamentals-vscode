9cd4d2bde5158cafe9a98848b8aec39161d5fc07
nightzillla <81122802+nightzillla@users.noreply.github.com>
2023-01-22 19:40:42 -0600

    finshed JS # 2

--- src/CodeWars/8kyu/8kyu.js
+++ src/CodeWars/8kyu/8kyu.js
@@ -3,3 +3,13 @@ function helloWorld (){
     return "Hello World!"
   }
   console.log(helloWorld);
+
+// Training JS #2: Basic data types--Number
+
+var v1=50;v2=100,v3=150,v4=200,v5=2,v6=250
+
+const equal1 = () => v1 + v1
+const equal2 = () => v3 - v1
+const equal3 = () => v1 * v5
+const equal4 = () => v4 / v5
+const equal5 = () => v2 % v4
