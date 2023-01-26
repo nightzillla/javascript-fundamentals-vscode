6c793c88224992dd4e2348792b9002d03260e1ed
nightzillla <81122802+nightzillla@users.noreply.github.com>
2023-01-25 21:22:31 -0600

    starting api calls with chuck

---
+++ src/js/chuck.js
@@ -0,0 +1,6 @@
+// async 
+// The async function declaration declares an async function where the await keyword is permitted within the function body. The async and await keywords enable asynchronous, promise-based behavior to be written in a cleaner style, avoiding the need to explicitly configure promise chains.
+
+async function fetchData() {
+
+}
