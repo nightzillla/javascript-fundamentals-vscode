776eec0448b57fc8c7459a57c64d35600b64ece7
nightzillla <81122802+nightzillla@users.noreply.github.com>
2023-01-27 14:44:39 -0600

    sovling odd and evens

--- src/js/practice.js
+++ src/js/practice.js
@@ -1,7 +1,16 @@
 let oddNumbers = [0,1,2,3,4,5,6,7,8,9,10]
 
+function findingEvenAndOdds(numbers){
+    for (let i = 0; i < numbers.length; i++){
+        if(numbers[i] % 2 == 0){
+            console.log(numbers[i] + "This is even")
+        } else {
+            console.log(numbers[i] + "This is odd")
+        }
+    } return numbers;
+}
 
-
+console.log(findingEvenAndOdds(oddNumbers));
 
 // function reverseArray(reverseArray){
 //     return reverseArray.reverse();
