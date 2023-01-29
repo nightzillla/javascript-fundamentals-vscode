661b04f9d2e7802fc2b20495d6305edfd71d2ccc
nightzillla <81122802+nightzillla@users.noreply.github.com>
2023-01-28 18:27:10 -0600

    null vs undefined

--- src/js/interviewQuestions.js
+++ src/js/interviewQuestions.js
@@ -9,3 +9,14 @@ var girl = function () {
 };
 console.log(x);       // output will be 21
 girl();
+ 
+/**
+ * TODO: What will be logged in first example and in second example? */
+// output will be undefine 
+let var1;
+console.log(var1);
+console.log(typeof var1);
+
+let var2 = null;
+console.log(var2);
+console.log(typeof var2);
