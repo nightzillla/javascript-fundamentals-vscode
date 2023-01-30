d7b3b2275027f313ca00113e5ae479d1e7b6c443
nightzillla <81122802+nightzillla@users.noreply.github.com>
2023-01-29 21:37:31 -0600

    4 pillars

--- src/js/interviewQuestions.js
+++ src/js/interviewQuestions.js
@@ -185,7 +185,7 @@ console.log(checkPalidrome("racecar"));
 /**
 ** Array
 * TODO: Write a function which get's an array and an element and returns a array with this element at the end */
-
+const numbers = [1, 2]
 const append = (arr, el) => {
     arr.push(el);
     return arr;
