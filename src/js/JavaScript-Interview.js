e19562817c91b6871ea8dd92f207f7bf849cb224
nightzillla <81122802+nightzillla@users.noreply.github.com>
2023-01-31 18:53:19 -0600

    learning primitive and non priimitive types

--- src/js/JavaScript-Interview.js
+++ src/js/JavaScript-Interview.js
@@ -4,5 +4,28 @@
 /**
  *! Primitive types
  /**
- ** String - it represents a series of characters and is written with quotes. A string can be represented using a single or a double quote. 
+ ** String - It represents a series of characters and is written with quotes. A string can be represented using a single or a double quote. 
+
+ ** Number - It represents a number and can be written with or without decimals
+
+ ** BigInt - This data type is used to store numbers which are above the limitation of the Number data type. It can store large integers and is represented by adding "n" to an integer literal. 
+
+ ** Boolean - It represents a logical entity and can have only two values: true or false.
+
+ ** Undefined - When a variable is declared but not assigned, it has the value of undefined and it's type is also undefined. 
+    // Example 
+    var x; // value of x is undefined
+    var y = undefined; 
+** Null - It represents a non-existent or a invalid value.
+    // Example 
+    var z = null;
+** Symbol - It is a new data type introduced in the ES6 version of javascript. It is used to store an anonymous and unique value. 
+    // Example 
+    var symboll = Symbol('symbol')
 */
+/**
+ *! Non-primitive types
+    ** Primitive data types can store only a single value. To store multiple and complex values, non-primitive data types are used. 
+    
+    ** Object - Used to store collection of data.
+ */
