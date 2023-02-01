0c3d8fdbb6cda7d11d10ade8933506c64e1e26a6
nightzillla <81122802+nightzillla@users.noreply.github.com>
2023-01-31 18:37:22 -0600

    learning javascript fundamentals

--- src/js/JavaScript-Interview.js
+++ src/js/JavaScript-Interview.js
@@ -1,1 +1,8 @@
-// What are the different data types present in javascript?
+/**
+ ** What are the different data types present in javascript?
+*/
+/**
+ *! Primitive types
+ /**
+ ** String - it represents a series of characters and is written with quotes. A string can be represented using a single or a double quote. 
+*/
