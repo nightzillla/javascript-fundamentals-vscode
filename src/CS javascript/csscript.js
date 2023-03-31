baf015ca6d53dec166ad811ec4948be2a8965f5f
nightzillla <81122802+nightzillla@users.noreply.github.com>
2023-03-31 15:14:27 -0500

    created function

--- src/CS javascript/csscript.js
+++ src/CS javascript/csscript.js
@@ -3,4 +3,23 @@ var myNumber = 5;
 
 // Send the value of the variable to the output console
 console.log(myNumber);
-console.log(typeof myNumber);
+console.log(typeof myNumber); 
+
+// Create the game score
+let myScore;
+
+// Setup and change the game score
+init();
+changeScore();
+
+// function to initialize the game score in the game
+function init(){
+    myScore = 1000;
+}
+
+// function to change the score of the game
+function changeScore() {
+    myScore = myScore + 100;
+    console.log("Player score: " + myScore);
+}
+
