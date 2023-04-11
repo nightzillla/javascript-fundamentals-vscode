09b48bd410d968469014dcff9519ec70d28a901f
nightzillla <81122802+nightzillla@users.noreply.github.com>
2023-04-11 05:03:47 -0500

    finshed functions with parameters

--- src/CS javascript/csscript.js
+++ src/CS javascript/csscript.js
@@ -10,17 +10,24 @@ let myScore;
 
 // Setup and change the game score
 init(1000);
-changeScore();
+changeScore(100);
+changeScore(50);
+changeScore(-250);
+changeScore(125);
 
 // function to initialize the game score in the game
 function init(newScore){
+
+    console.log("Setting the score to " + newScore);
     myScore = newScore;
     displayScore();
 }
 
 // function to change the score of the game
-function changeScore() {
-    myScore = myScore + 100;
+function changeScore(scoreDelta) {
+
+    console.log("Changing the score by " + scoreDelta);
+    myScore = myScore + scoreDelta;
     displayScore();
 }
 
